5ebd6c725d4afac8dd5d85da168dc3e7a89fd518
Bruno Alassia <vruno.alassia@gmail.com>
2011-08-23 18:36:46 -0300

    Tiny description

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,18 +12,41 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->webView->page()->settings()->setAttribute(QWebSettings::DeveloperExtrasEnabled, true);
     ui->webView->page()->settings()->setAttribute(QWebSettings::JavascriptEnabled, true);
 
-//  android size
-    ui->webView->setGeometry(QRect(0,0, 480, 800));
-    this->setGeometry(QRect(10,10, 480, 800));
+//  android size large
+//    ui->webView->setGeometry(QRect(0,0, 480, 800));
+//    this->setGeometry(QRect(10,10, 480, 800));
+
+//   ui->webView->setGeometry(QRect(0,0, 240, 320));
+//   this->setGeometry(QRect(10,10, 240, 320));
+
+//  android medium size - iphone 3
+//    ui->webView->setGeometry(QRect(0,0, 320, 480));
+//   this->setGeometry(QRect(10,10, 320, 480));
+
+// iphone 4
+//    ui->webView->setGeometry(QRect(0,0, 640, 960));
+//    this->setGeometry(QRect(10,10, 640, 960));
+
+// BB small size
+//   ui->webView->setGeometry(QRect(0,0, 320, 240));
+//   this->setGeometry(QRect(10,10, 320, 240));
+
+// BB  large size
+//    ui->webView->setGeometry(QRect(0,0, 360, 480));
+//    this->setGeometry(QRect(10,10, 360, 480));
 
 //  nokia size
-//    ui->webView->setGeometry(QRect(0,0, 360, 640));
-//    this->setGeometry(QRect(10,10, 360, 640));
+    ui->webView->setGeometry(QRect(0,0, 360, 640));
+    this->setGeometry(QRect(10,10, 360, 640));
 
-    /*
-    ui->webView->page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
-    ui->webView->page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
-    */
+//  nokia small size
+//    ui->webView->setGeometry(QRect(0,0, 240, 320));
+//    this->setGeometry(QRect(10,10, 240, 320));
+
+
+//    ui->webView->page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
+//    ui->webView->page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
+
 
     inspector = new QWebInspector;
     inspector->setPage(ui->webView->page());
--- ui_mainwindow.h
+++ ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created: Sun Jun 19 15:04:58 2011
+** Created: Sat Jul 30 13:45:03 2011
 **      by: Qt User Interface Compiler version 4.7.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
